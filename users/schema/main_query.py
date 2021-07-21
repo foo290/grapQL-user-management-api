ad9ad7343412f452b69f7f3dbd6f1e34a05b6e05
foo290 <ns290670@gmail.com>
2021-07-21 21:30:22 +0530

    docs

--- users/schema/main_query.py
+++ users/schema/main_query.py
@@ -6,8 +6,72 @@ from graphql_auth.schema import UserQuery, MeQuery
 
 class QueryType(UserQuery, MeQuery, graphene.ObjectType):
     """
-    This is the schema root, everything here is handled in parent classes.
-    """
+
+    This is the root of mutation schema, This class is used in main_query for exposing mutable endpoints.
+
+    # Creating a User:
+    ------------------------------------------------
+        mutation{
+          register(
+            email:"myEmail@gmail.com"
+            username:"myUsername"
+            password1:"testing321"
+            password2:"testing321"
+          )
+          {
+            success
+            errors
+            token
+            refreshToken
+          }
+        }
+
+    # Reading data:
+    ------------------------------------------------
+        query{
+            users(username:"admin"){
+            edges{
+              node{
+                username
+                lastName
+                firstName
+                lastLogin
+              }
+            }
+          }
+        }
+
+        query{
+          me{
+            lastName
+            firstName
+            verified
+            isActive
+          }
+        }
+
+    # Updating user
+    -------------------------------------------------
+        ##Needs JWT token as authentication header for unauthenticated accounts
+        mutation{
+          updateUser(firstName:"new name")
+          {
+            success
+            errors
+          }
+        }
+
+    #Delete User
+    --------------------------------------------------
+        ##Will delete the current logged in account
+        mutation{
+          deleteUser(password:"mypw"){
+            success
+            errors
+          }
+        }
+
+      """
     pass
 
 
